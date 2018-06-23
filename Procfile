21bff0afbef87493adf771608320a75b3ce83353
Junior Gantin <nioperas06@gmail.com>
2018-06-23 21:22:01 +0100

    Add Heroku config

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: ENV_SILENT=true node ace migration:run --force
+web: ENV_SILENT=true npm start
--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@
 
 const Env = use('Env')
 const Helpers = use('Helpers')
+const URL = require('url-parse')
+const CLEARDB_DATABASE_URL = new URL(Env.get('CLEARDB_DATABASE_URL'))
 
 module.exports = {
   /*
@@ -47,11 +49,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
     }
   },
 
